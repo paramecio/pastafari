6295a4d450a3fd4a61fb8be7a9a92c82c7b97638
Antonio de la Rosa <webmaster@web-t-sys.com>
2016-06-19 05:21:10 +0200

    Little fix in routes of mariadb scripts

--- tasks/servers/databases/mysql/standalone_mysql.py
+++ tasks/servers/databases/mysql/standalone_mysql.py
@@ -9,7 +9,7 @@ class MakeTask(ArgsTask):
         
         super().__init__(conn)
         
-        self.files=[['modules/pastafari/scripts/servers/databases/mariadb/install_mariadb.py', 0o700]]
+        self.files=[['modules/pastafari/scripts/servers/databases/mariadb/${os_server}/install_mariadb.py', 0o700]]
         
         # Format first array element is command with the interpreter, the task is agnostic, the files in os directory. The commands are setted with 750 permission.
         # First element is the file, next elements are the arguments
