aec0517c9f9497081933bc6f91e893223359d997
Antonio de la Rosa <webmaster@web-t-sys.com>
2016-08-23 05:44:26 +0200

    Fixes in servers

--- models/servers.py
+++ models/servers.py
@@ -188,7 +188,7 @@ class DataServer(WebModel):
         self.register(corefields.ForeignKeyField('server_id', Server(connection), size=11, required=True, identifier_field='id', named_field="hostname", select_fields=['actual_idle', 'date']))
         self.register(corefields.ForeignKeyField('net_id', StatusNet(connection), size=11, required=False, identifier_field='id', named_field="bytes_sent", select_fields=['bytes_sent', 'bytes_recv']))
         self.register(corefields.ForeignKeyField('memory_id', StatusMemory(connection), size=11, required=False, identifier_field='id', named_field="free", select_fields=['free', 'used', 'cached']))
-        self.register(corefields.ForeignKeyField('cpu_id', StatusCpu(connection), size=11, required=False, identifier_field='id', named_field="idle", select_fields=[]))
+        self.register(corefields.ForeignKeyField('cpu_id', StatusCpu(connection), size=11, required=False, identifier_field='id', named_field="idle", select_fields=['num_cpu']))
         
         self.register(corefields.ForeignKeyField('disk0_id', StatusDisk(connection), size=11, required=False, identifier_field='id', named_field="disk", select_fields=['free', 'used', 'size', 'percent']))
         
