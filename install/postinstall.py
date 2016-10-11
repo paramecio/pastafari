c343f0bde34fe120a7c1c007396d1a60bb8100d7
Antonio de la Rosa <webmaster@web-t-sys.com>
2016-10-11 04:54:45 +0200

    Fix in install scripts

--- install/postinstall.py
+++ install/postinstall.py
@@ -16,8 +16,8 @@ from settings import config
 from subprocess import call
 import getpass
 
-src='modules/pastafari/install/files/config_admin.py'
-dst='settings/config_admin.py'
+#src='modules/pastafari/install/files/config_admin.py'
+#dst='settings/config_admin.py'
 
 src_launcher='modules/pastafari/install/files/launcher.py'
 dst_launcher='launcher.py'
@@ -25,8 +25,12 @@ dst_launcher='launcher.py'
 src_scheduler='modules/pastafari/install/files/scheduler.py'
 dst_scheduler='scheduler.py'
 
+#Dont need copy admin configuration
+
+"""
 if not shutil.copy(src, dst):
     print('Error: cannot copy the admin configuration')
+"""
     
 if not shutil.copy(src_launcher, dst_launcher):
     print('Error: cannot copy the launcher')
