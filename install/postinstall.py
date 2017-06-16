4a9ba6bc779c1887093b4faee9626192a53b64af
Antonio de la Rosa <webmaster@web-t-sys.com>
2017-06-16 04:30:40 +0200

    Fix in postinstall

--- install/postinstall.py
+++ install/postinstall.py
@@ -15,9 +15,10 @@ from modules.pastafari.models import servers
 from settings import config
 from subprocess import call
 import getpass
+import pwd
 
-src='modules/pastafari/install/files/config_admin.py'
-dst='settings/config_admin.py'
+#src='modules/pastafari/install/files/config_admin.py'
+#dst='settings/config_admin.py'
 
 src_launcher='modules/pastafari/install/files/launcher.py'
 dst_launcher='launcher.py'
@@ -25,8 +26,12 @@ dst_launcher='launcher.py'
 src_scheduler='modules/pastafari/install/files/scheduler.py'
 dst_scheduler='scheduler.py'
 
+#Dont need copy admin configuration
+
+"""
 if not shutil.copy(src, dst):
     print('Error: cannot copy the admin configuration')
+"""
     
 if not shutil.copy(src_launcher, dst_launcher):
     print('Error: cannot copy the launcher')
@@ -125,7 +130,10 @@ with open('modules/pastafari/install/files/crontab') as f:
     cron_file=f.read()
     
 cron_file=cron_file.replace('/path/to/pastafari', working_directory)
-cron_file=cron_file.replace('pzoo', os.getlogin())
+#cron_file=cron_file.replace('pzoo', os.getlogin())
+
+user_login=pwd.getpwuid(os.getuid())[0]
+cron_file=cron_file.replace('pzoo', user_login)
 
 with open('modules/pastafari/install/files/crontab', 'w') as f:
     f.write(cron_file)
